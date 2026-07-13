ea1f2d3b3d3aa035d143c58e126097d47a4a7fd3
TruongDv-006 <tuthiengha@gmail.com>
2026-07-13 22:02:59 +0700

    feat: update scraper to support PDF and document downloading and add project configuration

--- worker/crawler/scraper.py
+++ worker/crawler/scraper.py
@@ -7,9 +7,10 @@ from urllib.parse import urljoin, urlparse
 
 # Đường dẫn cần save sau khi scraping xong
 SAVE_DIR = "../../infrastructure/volumes/minio_data/raw_web"
+DOCS_DIR = "../../infrastructure/volumes/minio_data/documents"
 
 os.makedirs(SAVE_DIR, exist_ok=True)
-
+os.makedirs(DOCS_DIR,exist_ok=True)
 
 class UetHandbookScraper:
     def __init__ (self, base_url):
@@ -29,19 +30,19 @@ class UetHandbookScraper:
     
     def is_valid_url(self, url):
         """ Ham nay kiem tra xem url nay co phai la 1 url can tham khong
-            Có 2 trường hợp cần phải loại bỏ:
-                1. Nếu tên domain không trùng với tên domain của miền gốc
-                2. Các đường link dẫn tới file tài liệu tĩnh
         """
         parsed = urlparse(url)
-        if parsed.netloc != self.domain:
-            return False
-        
-        if url.lower().endswith(('.pdf', '.doc', '.docx', '.jpg', '.png', '.zip')):
-            return False
+        netloc = parsed.netloc.lower()
 
-        return True
+        if netloc == self.domain:
+            if url.lower().endswith(('.jpg', '.png', '.zip', '.rar', '.mp4')):
+                return False
+            return True
 
+        if "vnu.edu.vn" in netloc:
+            if url.lower().endswith(('.pdf', '.docx', '.doc')) or any(keyword in url.lower() for keyword in ['/upload/', '/vanban/', '/data/']):
+                return True
+        return False
     def extract_content(self, soup):
         """ Hàm này dùng để trích xuất những nội dung cần dùng
             Đồng thời loại bỏ những thẻ thừa, nội dung không cần thiết
@@ -76,35 +77,51 @@ class UetHandbookScraper:
             if current_url in self.visited_url:
                 continue
             try:
-                # Gửi yêu cầu tới trang web quá 10s thì ngắt bỏ
                 response = self.session.get(current_url, timeout=10)
-                response.encoding = 'utf-8'
                 if response.status_code == 200:
+                    """Xử lý các file .pdf .docx và .docx
+                    """ 
+                    if current_url.lower().endswith(('.pdf','.doc','docx')):
+                        file_name = current_url.split('/')[-1]
+                        file_path = os.path.join(DOCS_DIR, file_name)
+
+                        with open(file_path, 'wb') as f:
+                            f.write(response.content)
+                        print(f"Đã tải thành công file đính kèm: {file_name}")
+                        self.visited_url.add(current_url)
+                        count+=1
+                        continue
+
+                    """Xử lý tiếp nếu không phải các file tĩnh
+                    """
+                    response.encoding = 'utf-8'
                     soup = BeautifulSoup(response.text,'html.parser')
 
-                    title = soup.title.string if soup.title else "no-title"
+                    # Đảm bảo cả soup.title và soup.title.string đều tồn tại thì mới lấy, không thì lấy "no-title"
+                    title = soup.title.string if (soup.title and soup.title.string) else "no-title"
                     text_content = self.extract_content(soup)
                     if text_content.strip():
                         # Làm sạch tên tiêu đề (xóa ký tự lạ) để đặt tên file không bị lỗi hệ điều hành
                         safe_title = "".join([c for c in title if c.isalnum() or c in (' ', '-')]).strip()
                         file_name = f"{safe_title}.json"
                         file_path = os.path.join(SAVE_DIR, file_name)
-                    #Dong goi du lieu
-                    data = {
-                        "url" : current_url,
-                        "title": title,
-                        "content": text_content
-                    }
-                    with open(file_path, 'w', encoding='utf-8') as f:
-                        json.dump(data, f, ensure_ascii=False, indent=4)
-                    #Danh dau da tham 
+                        # Dong goi du lieu
+                        data = {
+                            "url" : current_url,
+                            "title": title,
+                            "content": text_content
+                        }
+                        with open(file_path, 'w', encoding='utf-8') as f:
+                            json.dump(data, f, ensure_ascii=False, indent=4)
+                        count += 1
+                    
+                    # Danh dau da tham 
                     self.visited_url.add(current_url)
-                    count+=1
-                    #Lấy các đường dẫn từ trang hiện tại ra cào tiếp
-                    for link in soup.find_all('a', href = True):
+                    # Lấy các đường dẫn từ trang hiện tại ra cào tiếp
+                    for link in soup.find_all('a', href=True):
                         full_link = urljoin(current_url, link['href']).split('#')[0]
                         if self.is_valid_url(full_link) and full_link not in self.visited_url and full_link not in self.urls_visit:
-                            self.urls_visit.append(full_link)
+                                self.urls_visit.append(full_link)
                 time.sleep(1)
             except Exception as e:
                 print(f"Loi khi dang cao {current_url}:{e}")
