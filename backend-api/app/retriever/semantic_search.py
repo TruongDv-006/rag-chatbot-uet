bb40c665a0361c46dbaa9dbd5ce55399647bc16b
TruongDv-006 <tuthiengha@gmail.com>
2026-07-16 15:29:40 +0700

    feat: implement Qdrant-based semantic search retriever and add qdrant-client dependency

---
+++ backend-api/app/retriever/semantic_search.py
@@ -0,0 +1,30 @@
+import os
+from qdrant_client import  QdrantClient
+
+
+class SemanticRetriever:
+    client: QdrantClient
+
+    def __init__(self, qdrant_url = None, collection_name = "uet_handbook"):
+        if qdrant_url is None :
+            qdrant_url = os.getenv("QDRANT_URL", "http://localhost:6333")
+
+        self.client = QdrantClient(url=qdrant_url)
+        self.collection_name = collection_name
+    
+
+    def search(self, query_vector, top_k=5):
+        search_result = self.client.search( # pyrefly: ignore
+            collection_name=self.collection_name,
+            query_vector = query_vector,
+            limit = top_k
+        )
+        results = []
+        for hit in search_result:
+            results.append({
+                "doc_id" : hit.id,
+                "content": hit.payload.get("text",""),
+                "score": hit.score
+            })
+
+        return results
