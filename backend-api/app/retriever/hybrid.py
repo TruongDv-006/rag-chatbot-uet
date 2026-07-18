49dea006455575547d9b400705222a007ec5bc23
TruongDv-006 <tuthiengha@gmail.com>
2026-07-18 16:00:53 +0700

    feat: implement HybridRetrieve class with semantic search, keyword retrieval, and rank fusion logic

--- backend-api/app/retriever/hybrid.py
+++ backend-api/app/retriever/hybrid.py
@@ -1,4 +1,3 @@
-
 from qdrant_client import QdrantClient
 from app.retriever.semantic_search import SemanticRetriever # pyrefly: ignore[missing-import]
 from app.retriever.keyword_search import KeywordRetriever # pyrefly: ignore[missing-import]
@@ -8,4 +7,69 @@ from app.retriever.reranker import ReRanker # pyrefly: ignore[missing-import]
 
 class HybridRetrieve:
 
-    pass
+    def __init__(self,qdrant_client : QdrantClient, collection_name: str, embed_model):
+        self.qdrant_client=qdrant_client
+
+        self.collection_name=collection_name
+
+        self.semantic_retriever = SemanticRetriever(
+            qdrant_url=qdrant_client,
+            collection_name=collection_name
+        ) 
+        document = self._scroll_all_documents_from_qdrant()
+        self.keyword_retriever = KeywordRetriever(
+            documents = document
+        )
+        self.rank_fusion = RankFusion(k=60)
+        self.reranker = ReRanker(self, model_name = embed_model)
+
+    def _scroll_all_documents_from_qdrant(self):
+        """
+        Hàm này dùng để kéo toàn bộ tài liệu từ qdrant về để làm tham số cho các phần sau xử lý
+        """        
+        documents = []
+        next_page_offset = None
+        while True:
+            respone, next_page_offset = self.qdrant_client.scroll(
+                collection_name=self.collection_name,
+                limit=100,
+                with_payload = True,
+                with_vectors=False,
+                offset=next_page_offset
+            )
+            for point in respone:
+                payload = point.payload if point.payload else {}
+                documents.append({
+                    "doc_id": str(point.id),
+                    "content": str(payload.get("text_content",""))
+                })
+            if next_page_offset is None:
+                break
+
+        return documents
+
+    def search(self, query:str, query_vector:list[float], top_k: int=5):
+        raw_top_k = top_k * 4
+
+        semantic_result = self.semantic_retriever.search(
+            query_vector,
+            raw_top_k
+        )
+
+        keyword_result = self.keyword_retriever.search(
+            query,
+            raw_top_k
+        )
+
+        fused_result = self.rank_fusion.reciprocal_rank_fusion(
+            semantic_result,
+            keyword_result
+        )
+
+        final_reranked_result = self.reranker(
+            query,
+            fused_result,
+            top_k
+        )
+
+        return final_reranked_result
