7205bcae71687a924defe793fdeca3ff79027294
TruongDv-006 <tuthiengha@gmail.com>
2026-07-19 02:22:23 +0700

    feat: implement Ollama LLM client using OpenAI interface and update generation method signature

--- backend-api/app/generation/llm_client.py
+++ backend-api/app/generation/llm_client.py
@@ -1,4 +1,5 @@
 # Interface và các class con kết nối đến Ollama/vLLM (Strategy Pattern)
+from typing import override
 from abc import ABC, abstractmethod
 from openai import OpenAI
 from app.generation.config import generation_config # pyrefly: ignore [missing-import]
@@ -10,7 +11,7 @@ class LLMClient(ABC):
     phải viết 1 hàm "generate"
     """
     @abstractmethod
-    def generate(self, messages:list[dict[str, str]]):
+    def generate(self, messages:list) -> str:
         pass
 
 
@@ -20,9 +21,28 @@ class OpenAICompatibleClient(LLMClient):
     và dùng thư viện openai để nói chuyện với Ollama
     """
     def __init__(self):
-        
-        pass
-
-    def generate(self, messages:list[dict[str, str]]):
-
-        pass
+        """
+        Khởi tạo 1 kết nối(Client) thông qua thư viện openai
+        Tuy nhiên không hướng ra Internet của OpenAI mà hướng vào "ollama" trong Docker
+        """
+        self.client = OpenAI(
+            base_url = generation_config.LLM_API_BASE,
+            api_key = generation_config.LLM_API_KEY
+        )
+        self.model_name = generation_config.MODEL_NAME
+    def generate(self, messages:list) -> str:
+        """
+        Hàm này gửi dữ liệu qua Ollama và nhận câu trả lời về
+        """
+        try:
+            #Gửi câu hỏi
+            response = self.client.chat.completions.create(
+                model = self.model_name,
+                messages= messages,
+                max_tokens=generation_config.MAX_TOKENS,
+                temperature=generation_config.TEMPERATURE
+            )
+            content = response.choices[0].message.content
+            return content.strip() if content else ""
+        except Exception as e:
+            raise RuntimeWarning(f"Lỗi kết nối đến Ollama Engine:{e}")
