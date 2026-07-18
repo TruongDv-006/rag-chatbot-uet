c0df9e5cbf4d7157b6fc4084bf2aa098d4f23295
TruongDv-006 <tuthiengha@gmail.com>
2026-07-18 11:23:47 +0700

    feat: implement document reranking logic using cross-encoder model in reranker.py

--- backend-api/app/retriever/reranker.py
+++ backend-api/app/retriever/reranker.py
@@ -7,6 +7,20 @@ class Reranker:
 
 
     def rerank(self, query: str, documents :list[dict], top_k:int=5):
+        """
+        Tái xếp hạng danh sách documents dựa trên query
+        documents: List các chunk sau khi đi qua bộ RRF
+        """
+        if not documents:
+            return []
+        #Tạo ra các cặp đầu vào cho Cross-Encoder (query, text)
+        pairs = [[query, doc["content"]] for doc in documents]
 
-        pass
+        scores = self.model.predict(pairs)
 
+        for i, score in enumerate(scores):
+            documents[i]["rerank_score"] = float(score)
+
+        sorted_documents = sorted(documents, key=lambda x:x["rerank_score"], reverse=True)
+        
+        return sorted_documents[:top_k]
