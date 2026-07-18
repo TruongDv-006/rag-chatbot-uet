15eb1d19f616a9265452622eb6e3fed838fe7866
TruongDv-006 <tuthiengha@gmail.com>
2026-07-19 01:17:06 +0700

    chore: update dependency versions and synchronize generation configuration settings

--- backend-api/app/generation/config.py
+++ backend-api/app/generation/config.py
@@ -1,1 +1,28 @@
 # Quản lý cấu hình (ngưỡng score, model name, endpoint)
+from pydantic_settings import BaseSettings
+    
+class GenerationConfig(BaseSettings):
+    #Tên mô hình đang sử dụng
+    MODEL_NAME:str = "qwen2.5:3b"
+
+    #Địa chỉ kết nối với Ollama trong mạng Docker
+    LLM_API_BASE:str = "http://ollama:11434/v1"
+    
+    #Chìa khóa ảo (do OLLAMA không bắt buộc nên ta để mặc định)
+    LLM_API_KEY:str = "ollama"
+    
+    #Ngưỡng điểm tin cậy để chống ảo tưởng (nếu score nhỏ hơn ngưỡng này thì kết luận không có trong tài liệu)
+    DEFAULT_SCORE_THRESHOLD:float = 0.60
+    
+    #Giới hạn độ dài câu trả lời tính theo token
+    MAX_TOKENS:int = 512
+    
+    #Độ sáng tạo của AI trong câu trả lời
+    TEMPERATURE:float = 0.1
+
+    class Config:
+        # Lệnh này bảo Python tự động tìm và đọc thêm các biến môi trường từ tệp .env (nếu có)
+        env_file = ".env"
+        extra = "ignore"
+
+generation_config = GenerationConfig()
