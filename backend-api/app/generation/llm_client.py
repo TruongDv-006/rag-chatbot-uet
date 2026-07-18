178c1dbcff5cfec7733a4d009eb3c26cadcfce73
TruongDv-006 <tuthiengha@gmail.com>
2026-07-19 02:01:43 +0700

    feat: implement RAGGenerator class for orchestration and retrieval quality validation

--- backend-api/app/generation/llm_client.py
+++ backend-api/app/generation/llm_client.py
@@ -1,1 +1,28 @@
 # Interface và các class con kết nối đến Ollama/vLLM (Strategy Pattern)
+from abc import ABC, abstractmethod
+from openai import OpenAI
+from app.generation.config import generation_config # pyrefly: ignore [missing-import]
+
+class LLMClient(ABC):
+    """
+    Interface định nghĩa chung cho tất cả các AI
+    Nó buộc các lớp con khi kế thừa từ interface này đều 
+    phải viết 1 hàm "generate"
+    """
+    @abstractmethod
+    def generate(self, messages:list[dict[str, str]]):
+        pass
+
+
+class OpenAICompatibleClient(LLMClient):
+    """
+    Lớp này kế thừa từ LLMClient
+    và dùng thư viện openai để nói chuyện với Ollama
+    """
+    def __init__(self):
+        
+        pass
+
+    def generate(self, messages:list[dict[str, str]]):
+
+        pass
--- backend-api/app/generation/orchestrator.py
+++ backend-api/app/generation/orchestrator.py
@@ -1,1 +1,20 @@
 # Class trung tâm điều phối RAG và lọc Anti-Hallucination
+from app.generation.config import generation_config # pyrefly: ignore [missing-import]
+from app.generation.prompt_manager import PromptManager # pyrefly: ignore [missing-import]
+from app.generation.llm_client import LLMClient # pyrefly: ignore [missing-import]
+
+class RAGGenerator:
+
+    def __init__(self, llm_client: LLMClient):
+        pass
+
+    def _validate_retrieval_quality(self, retrieved_docs:list[dict], threshold:float):
+        """
+        Hàm này kiểm tra xem có tài liệu nào lấy từ Vector DB có đoạn nào đủ tin cậy không
+        Điều này chống Anti-Hallucination
+        """
+    
+    def execute(self, query:str, retrieved_docs:list[dict], score_threshold: float):
+        """
+        Hàm thực thi chính của luồng RAG
+        """
