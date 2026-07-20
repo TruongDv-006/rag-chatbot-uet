b23f17b034515975436a92cbe242a9421b223771
TruongDv-006 <tuthiengha@gmail.com>
2026-07-20 16:56:36 +0700

    feat: implement JWT-based security, add role-based access control, and define chat session models.

--- backend-api/app/routes/admin.py
+++ backend-api/app/routes/admin.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from app.services.admin_service import AdminService # pyrefly: ignore
+from app.utils.security import get_current_admin # pyrefly:ignore
 
 router = APIRouter()
 
@@ -8,10 +9,11 @@ class UploadRequest(BaseModel):
     file_name:str
 
 @router.post("/upload-doc")
-def upload_document(request: UploadRequest, service: AdminService = Depends()):
+def upload_document(request: UploadRequest, service: AdminService = Depends(), current_admin: str = Depends(get_current_admin)):
+    print(f"[Admin API] Tài khoản {current_admin} đang đẩy file {request.file_name} vào hàng đợi Celery")
     task_id = service.queue_docment_processing(request.file_name)
-
     return {
         "message":"Tài liệu đã được đưa vào hàng đợi",
-        "task_id":task_id
+        "task_id":task_id,
+        "triggered_by": current_admin
     }
--- backend-api/app/routes/chat.py
+++ backend-api/app/routes/chat.py
@@ -2,15 +2,21 @@ from functools import lru_cache
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from app.services.chat_service import ChatService # pyrefly: ignore
+from app.utils.security import get_current_user # pyrefly: ignore
 
 router = APIRouter()
 class ChatRequest(BaseModel):
     message:str
+    session_id: int 
     
 @lru_cache()
 def get_chat_service():
     return ChatService()
 
 @router.post("/chat")
-def chat_with_rag(request: ChatRequest, service: ChatService=Depends(get_chat_service)):
-    return service.process_message(request.message)
+def chat_with_rag(request: ChatRequest, service: ChatService=Depends(get_chat_service), current_user: str = Depends(get_current_user)):
+    return service.process_message(
+        request.message, 
+        username=current_user,
+        session_id=request.session_id
+    )
