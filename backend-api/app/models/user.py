df07aff53d513477d8eeb3289cc0c471f26d7775
TruongDv-006 <tuthiengha@gmail.com>
2026-07-21 22:47:33 +0700

    feat: add created_at field to User model for tracking user creation time

--- backend-api/app/models/user.py
+++ backend-api/app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, Column, String # pyrefly: ignore
+from sqlalchemy import Integer, Column, String, DateTime, func # pyrefly: ignore
 from app.core.database import Base # pyrefly: ignore
 
 class User(Base):
@@ -10,3 +10,4 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     full_name = Column(String, nullable=False)
     role = Column(String, default="student", nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
